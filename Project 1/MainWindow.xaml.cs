cbf4fdfb8d4133ae1573ffb2ef02ecc848671b18
Antonio <antonioareas@me.com>
2020-09-20 14:17:15 -0700

    Add project files.

---
+++ Project 1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //declare interest rate
+        public const double YEARLY_INTEREST_RATE = .0639;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            //declare textbox variables
+            double monthlyPayment = Double.Parse(paymentTextBox.Text);
+            double outstandingPayment = Double.Parse(outstandingTextBox.Text);
+
+            //call function
+            calculatePayment(monthlyPayment, outstandingPayment);
+
+        }
+
+        static void calculatePayment(double monthlyPayment, double oustandingPayment)
+        {
+
+            //calculate monthly rate
+            double monthlyInterestRate = (1.0/12.0) * YEARLY_INTEREST_RATE;
+
+            //calculate interest paid
+            double interestPaid = oustandingPayment * monthlyInterestRate;
+
+            //calculate principal paid
+            double principalPaid = monthlyPayment - interestPaid;
+
+            //output results
+            MessageBox.Show("You paid $" + principalPaid + " in principal and $" + interestPaid + " in interest this month.");
+
+        }
+
+    }
+}
---
+++ Project 2/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_click(object Sender, RoutedEventArgs e)
+        {
+
+            //get price from user input
+            int price = Int32.Parse(priceTextBox.Text);
+
+            //call function
+            calculateChange(price);
+
+
+
+        }
+
+        public void calculateChange(int price)
+        {
+
+            //declare coin variables
+            int quarters, dimes, nickels;
+
+            //calculate quarters required and subtract from price
+            quarters = price / 25;
+            price = price - quarters * 25;
+
+            //calculate dimes required and subtract from price
+            dimes = price / 10;
+            price = price - dimes * 10;
+
+            //calculate nickels required and subtract from price
+            nickels = price / 5;
+            price = price - nickels * 5;
+
+            //change labels to reflectvalues
+            quartersLabel.Content = "Quarters: " + quarters;
+            dimesLabel.Content = "Dimes: " + dimes;
+            nickelsLabel.Content = "Nickels: " + nickels;
+
+        }
+
+    }
+}
